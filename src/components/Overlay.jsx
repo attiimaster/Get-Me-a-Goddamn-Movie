1228af7751fd22be5cf0bb8cca5d4b576bc27061
Atti <raphael_mayer@hotmail.de>
2018-05-27 18:07:58 +0200

    improved visibility

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -12,34 +12,34 @@ const Overlay = (props) => {
 	
 	return(
 		<div className='overlay'>
-			
-			<div className='backdrop' style={{ backgroundImage: 'url(' + url + details.backdrop_path + ')' }}>
-				
-				<h2 className='overlay-title'>{ details.original_title } ({ details.release_date.slice(0, 4) })
-
-				</h2>
-
-				<div className='overlay-runtime'> 
-					<Stars votes={ details.vote_average } />
-					{ Math.floor(details.runtime / 60) + ':' }{ details.runtime % 60 + 'h' }
+			<div className='overlay-inner'>
+				<div className='backdrop' style={{ backgroundImage: 'url(' + url + details.backdrop_path + ')' }}>
+					<div class='backdrop-filter'>
+						<h2 className='overlay-title'>{ details.original_title } ({ details.release_date.slice(0, 4) })
+		
+						</h2>
+		
+						<div className='overlay-runtime'> 
+							<Stars votes={ details.vote_average } />
+							{ Math.floor(details.runtime / 60) + ':' }{ details.runtime % 60 + 'h' }
+						</div>
+		
+						<h3 className='overlay-tagline'>{ details.tagline }</h3>			
+					
+						<div className='overlay-text'>		
+							{ details.overview }
+						</div>
+					</div>
+				</div>			
+	
+				<div className='overlay-img-container'>{ images.map((item, i) => <div className='overlay-imgs' key={i} style={{ backgroundImage: 'url(' + url + item.file_path + ')' }}></div>)}</div>
+					
+				<div className='watch-trailer' onClick={ () => redirectToYouTube(props.original_title) }>
+					<i className='fas fa-desktop'></i>
 				</div>
-
-				<h3 className='overlay-tagline'>{ details.tagline }</h3>			
-			
-				<div className='overlay-text'>		
-					{ details.overview }
-				</div>
-				
-			</div>			
-
-			<div className='overlay-img-container'>{ images.map((item, i) => <div className='overlay-imgs' key={i} style={{ backgroundImage: 'url(' + url + item.file_path + ')' }}></div>)}</div>
-				
-			<div className='watch-trailer' onClick={ () => redirectToYouTube(props.original_title) }>
-				<i className='fas fa-desktop'></i>
+	
+				<i onClick={ () => props.close() } className='far fa-times-circle'></i>
 			</div>
-
-			<i onClick={ () => props.close() } className='far fa-times-circle'></i>
-
 		</div>
 	)
 }
